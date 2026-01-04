bbfeb882c9ec8bc86f75e17d215713ff86182b4f
Barbara Poblegte <bpobletem96@gmail.com>
2026-01-04 12:41:40 -0300

    del self + add type

--- app/files/models.py
+++ app/files/models.py
@@ -4,9 +4,6 @@ from app.files.enums import TransactionType
 class Asset(models.Model):
     name = models.CharField(max_length=30, null=False)
 
-    def __str__(self):
-        return f"{self.name}"
-
 class AssetPrice(models.Model):
     asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
     date = models.DateField(null=False)
@@ -15,15 +12,9 @@ class AssetPrice(models.Model):
     class Meta:
         unique_together = ("asset", "date")
 
-    def __str__(self):
-        return f"{self.date} -- {self.asset} - {self.price}"
-
 class Portfolio(models.Model):
     name = models.TextField(max_length=20, null=False)
     initial_value = models.DecimalField(max_digits=20, decimal_places=10)
-
-    def __str__(self):
-        return f"{self.name}"
 
 class PortfolioAsset(models.Model):
     portfolio = models.ForeignKey(Portfolio, on_delete=models.PROTECT)
@@ -34,14 +25,10 @@ class PortfolioAsset(models.Model):
 
     class Meta:
         unique_together = ("portfolio", "asset", "initial_date")
-        
-    def __str__(self):
-        return f"{self.portfolio} - {self.asset} - {self.date} - {self.weight}"
 
 class Transaction(models.Model):
-    type = models.CharField(max_length=10) #agregar enum
+    type = models.CharField(max_length=7, choices=TransactionType.choices)
     portfolio = models.ForeignKey(Portfolio, on_delete=models.PROTECT)
     asset = models.ForeignKey(Asset, on_delete=models.PROTECT)
     date = models.DateField()
     value = models.DecimalField(max_digits=20, decimal_places=10)
-    quantity = models.DecimalField(max_digits=20, decimal_places=10)
