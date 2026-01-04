08b114e21d2b1c49301d69f2972e058ddb24d789
Barbara Poblegte <bpobletem96@gmail.com>
2026-01-04 12:42:12 -0300

    edit urls

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,6 @@ from .files.apis import *
 from .files.views import *
 
 urlpatterns = [
-    path('health/', HealthCheckApi.as_view(), name='health-check'),
     path('api/evolution', PortfolioEvolutionApi.as_view(), name='portfolio_evolution'),
     path('dashboard/', PortfolioDashboardView.as_view(), name='dashboard'),
 ]
